7e18816cece8f66340b6ee67f97831fbfdb243c3
kyoohyun <kgh2120@gmail.com>
2023-10-31 10:20:34 +0900

    init pjt

---
+++ src/main/java/com/qq/samplecodeforej/SampleCodeForEjApplication.java
@@ -0,0 +1,13 @@
+package com.qq.samplecodeforej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCodeForEjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleCodeForEjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qq/samplecodeforej/SampleCodeForEjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qq.samplecodeforej;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleCodeForEjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
